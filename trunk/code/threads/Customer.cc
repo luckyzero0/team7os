e48ba53a20247233ad1d9236b242734a377d8ef3
cramer.jay <cramer.jay@1c0c25f8-55f5-9bb9-4c71-649d011553c2>
2011-02-03 02:12:54 +0000

    Print statements and more updates

--- trunk/code/threads/Customer.cc
+++ trunk/code/threads/Customer.cc
@@ -2,33 +2,53 @@
 #include "office.h"
 
 void CustomerRun(){
-	
+	cout<<"Entering the passport office..."<<endl;
 	//customer start up code	
 	srand( time(NULL) );
 	int cashDollars = ((rand() % 3) * 500) + 100;
+	cout<<"With $"<<cashDollars<<" in my pocket"<<endl;
 	//acquire ACPCLock
 	appPicLineLock->Acquire();	
 	totalCustomersInOffice++;
 	
 	//choose line		
+	cout<<"Deciding between AppClerk and PictureClerk..."<<endl;
 		
 	//app clerk		
+	cout<<"Going to the AppClerk"<<endl;
 	while(true)
 	{		
+		cout<<"What line should I choose for the AppClerk?"<<endl;
 		//check for senator
 		if(cashDollars > 100) //get in a privledged line
-		{			
+		{						
 			cashDollars -= 500;
+			cout<<"Priveleged line, baby. CashDollars = $"<<cashDollars<<endl 
 			privAppLineLength++;
+			cout<<"Waiting in the Priveledged Line for next available clerk"<<endl;
 			privAppLineCV->Wait(appPicLineLock);			
 		}
 		else //get in a normal line
 		{
+			cout<<"Regular line, suckahs. CashDollars = $"<<cashDollars<<endl;
 			regAppLineLength++;
+			cout<<"Waiting in the Regular Line for next available clerk"<<endl;
 			regAppLineCV->Wait(appPicLineLock);			
-		}						
+		}				
+		cout<<"Finding available clerk..."<<endl;		
+		for(int x = 0; x < (sizeof(appClerkStatuses)/sizeof(appClerkStatuses[0])); x++)
+		{
+			if(appClerkStatuses[x] == ClerkStatus.CLERK_AVAILABLE)
+			{
+				cout<<"Going to chill with App Clerk #"<<x<<endl;
+				appPicLineLock->Release();			
+				break;				
+			}
+		}
+		cout<<"Interacting with app clerk"<<endl;
 		//interact with clerk
-		appPicLineLock->Release();
+		cout<<"Done and done."<<endl;
+		
 		break;
 	}	
 	
