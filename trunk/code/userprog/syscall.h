22f5f58c334e3fc086c8aea5030406b57bb19c83
mihirshe@gmail.com <mihirshe@gmail.com@1c0c25f8-55f5-9bb9-4c71-649d011553c2>
2011-02-18 06:09:35 +0000

    Added the SC_Constants for all our Syscalls, and implemented Exit and Yield.  Exit may be way off the mark, but we think it works.  Also added a HasThreadsRemaining() function to the Scheduler.

--- trunk/code/userprog/syscall.h
+++ trunk/code/userprog/syscall.h
@@ -18,17 +18,26 @@
 /* system call codes -- used by the stubs to tell the kernel which system call
  * is being asked for
  */
-#define SC_Halt		0
-#define SC_Exit		1
-#define SC_Exec		2
-#define SC_Join		3
-#define SC_Create	4
-#define SC_Open		5
-#define SC_Read		6
-#define SC_Write	7
-#define SC_Close	8
-#define SC_Fork		9
-#define SC_Yield	10
+#define SC_Halt				0
+#define SC_Exit				1
+#define SC_Exec				2
+#define SC_Join				3
+#define SC_Create			4
+#define SC_Open				5
+#define SC_Read				6
+#define SC_Write			7
+#define SC_Close			8
+#define SC_Fork				9
+#define SC_Yield			10
+#define SC_CreateLock		11
+#define SC_DestroyLock		12
+#define SC_CreateCondition	13
+#define SC_DestroyCondition	14
+#define SC_Acquire			15
+#define SC_Release			16
+#define	SC_Signal			17
+#define SC_Wait				18
+#define SC_Broadcast		19
 
 #define MAXFILENAME 256
 
@@ -124,7 +133,10 @@ void Fork(void (*func)());
 /* Yield the CPU to another runnable thread, whether in this address space 
  * or not. 
  */
-void Yield();		
+void Yield();
+
+////// START OUR ADDITIONS
+
 
 #endif /* IN_ASM */
 
