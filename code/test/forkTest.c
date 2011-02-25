963e24d92a8c492efa3fdb8392f78d104544fa2c
cramer.jay <cramer.jay@1c0c25f8-55f5-9bb9-4c71-649d011553c2>
2011-02-25 22:24:27 +0000

    Print working and added to utils.

--- code/test/forkTest.c
+++ code/test/forkTest.c
@@ -2,23 +2,31 @@
 Test for forking a thread. If successful, x = 10*/
 
 #include "syscall.h"
-
+#include "utils.h"
 int x = 0;
 void testProc();
 int main(){
-
+	
+	
 	/*Fork a thread*/
+	char* msg = "Forking thread 1...\n";
+	Write(msg,strlen(msg),ConsoleOutput);
+	print("Also testing a print %d suckas!\n", 49,0,0,"s","t");
+	
 	Fork(testProc);
 	
 	/*fork another thread*/
+	msg = "Forking thread 2...\n";
+	Write(msg,strlen(msg),ConsoleOutput);
 	Fork(testProc);
+		
 
   return 0;
 }
 
 
 void testProc(){
-	write("Entering testProc",17);
+	Write("Entering testProc",17, ConsoleOutput);
 	x+=5;
 }
 	
