267bbcf945dccef5f910d8c1223bf12091c54776
cramer.jay <cramer.jay@1c0c25f8-55f5-9bb9-4c71-649d011553c2>
2011-02-25 22:25:12 +0000

    forgot to add utils

---
+++ code/test/utils.h
@@ -0,0 +1,9 @@
+/*utils.h*/
+
+extern int strlen(char* str);
+
+extern void print(char* str, int arg1, int arg2, int arg3, char* arg4, char* arg5);
+
+extern void itoa(int n, char s[]);
+
+extern void reverse(char s[]);
