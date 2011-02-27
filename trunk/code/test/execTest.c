f608b6efbc0d646739f671d317753a7c7e99f1c6
cramer.jay <cramer.jay@1c0c25f8-55f5-9bb9-4c71-649d011553c2>
2011-02-27 02:12:58 +0000

    added exectest

---
+++ trunk/code/test/execTest.c
@@ -0,0 +1,34 @@
+/*execTest.c*/
+#include "syscall.h"
+#include "utils.h"
+
+
+int main(){
+	char* file;
+	printf("Trying to exec a non-existent file...\n",0,0,0,"","");	
+	file = "../test/BADFILENAMELOLZ";
+	Exec(file,strlen(file));
+	
+	printf("Trying to exec a non-binary file...\n",0,0,0,"","");
+	file = "../test/testLocks.c";
+	Exec(file, strlen(file));
+	
+	printf("Trying to exec with bad strLength...\n",0,0,0,"","");
+	file = "../test/forkTest";
+	Exec(file,2);
+	
+	
+	printf("It isn't enough, we need to go deeper.\n",0,0,0,"","");
+	file = "../test/exec_testLocks";
+	Exec(file,strlen(file));
+	
+	printf("And finally a normal Exec.\n",0,0,0,"","");
+	file = "../test/forkTest";
+	Exec(file,strlen(file));
+	
+	printf("All Exec() tests executed.\n",0,0,0,"","");
+	
+	Exit(0);
+}
+
+
