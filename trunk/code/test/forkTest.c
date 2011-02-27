a59451703cbddf13976b6ba85e426fe78e3345be
cramer.jay <cramer.jay@1c0c25f8-55f5-9bb9-4c71-649d011553c2>
2011-02-27 01:30:18 +0000

    updated forktest

--- trunk/code/test/forkTest.c
+++ trunk/code/test/forkTest.c
@@ -5,6 +5,10 @@ Test for forking a thread. If successful, x = 10*/
 #include "utils.h"
 
 int x = 0;
+int i = 0;
+int null = 0;
+int bad = 3286987;
+int unaligned = 13;
 void testProc();
 int main(){
 
@@ -15,7 +19,26 @@ int main(){
 
 	/*fork another thread*/
 	printf("Forking thread 2...\n",0,0,0,"","");	
-	Fork(testProc);			
+	Fork(testProc);					
+		
+	printf("Trying to pass bad(out of bounds) function pointers...\n",0,0,0,"","");
+	Fork(bad);
+	Fork(-1);
+	
+	printf("Fork bombing the system...\n",0,0,0,"","");
+	Fork(null);
+	
+	/*printf("Trying to pass an unaligned function address...\n",0,0,0,"","");
+	Fork(unaligned);*/
+
+	printf("WHAT THE FUCK!?",0,0,0,"","");
+	printf("Trying to pass generically bad fnAddresses from 0 to [0x%x]...\n",testProc,0,0,"","");
+	/*while(i < testProc)
+	{
+		printf("Trying to pass generically bad function addresses[0x%x]...\n",i,0,0,"","");
+		Fork(i);
+		i++;
+	}*/
 
 	Exit(0);
 }
